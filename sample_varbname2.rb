d974707c8d1b47884a18e58b300c9e5234568250
Rishi <rishikumar.bokka@gmail.com>
2015-03-03 19:08:14 +0530

    Program on variable names

---
+++ sample_varbname2.rb
@@ -0,0 +1,17 @@
+name = 'B Rishi Kumar'
+age = 23 # not a lie in 1991
+height = 5.6 # inches
+weight = 62 # kgs
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} kgs heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
