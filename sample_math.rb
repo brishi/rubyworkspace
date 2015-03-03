89957c2660c8031ad2190a86c6ba7426a618079d
Rishi <rishikumar.bokka@gmail.com>
2015-03-03 18:04:58 +0530

    Program of simple maths operations

---
+++ sample_math.rb
@@ -0,0 +1,38 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2} \n \n "
+
+puts "I will know check how all maths operations work"
+
+puts "Add and division of 25 + 30 / 6  = #{25 + 30 / 6}"
+puts "subration ,  multiplication and division  of 100 - 25 * 3 % 4 = #{100 - 25 * 3 % 4}"
+
+puts 7 + 4 + 8 + 1 + 1 + 1 % 3 + 9 % 6
+
+puts "IS it true that 5 + 5 > 3 + 3"
+puts 5 + 5 > 3 + 3
+
+puts "What is 9 + 9? #{9 + 9} "
+puts "What is 5 * 5? #{5 * 5}"
+
+puts "Is it greater? #{-7 > -2}"
