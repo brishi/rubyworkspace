91d120214ead22c999895e10507c850daaab34c5
Rishi <rishikumar.bokka@gmail.com>
2015-03-03 17:42:06 +0530

    Program for Comments

---
+++ sample_comment.rb
@@ -0,0 +1,6 @@
+# Hi this is a comment
+#It doesnt get printed on the output
+# It helps the viewer to understand what the program is about
+puts "Program to check comment works or not??"
+puts "Yes it works well"#  It doesnt get printed
+puts "Only # in this statement gets printed"
