93f17182ec8ca0b548affbdd98b9cbce0c77881b
Rishi <rishikumar.bokka@gmail.com>
2015-03-05 12:32:05 +0530

    Program on strings

---
+++ sample_string.rb
@@ -0,0 +1,16 @@
+no_of_apples = 50
+x = "They have #{no_of_apples} with them"
+spoiled_apples = 20
+flaw = "flaw"
+y = "amoung them #{spoiled_apples} are #{flaw} "
+ 
+ puts x
+ puts y
+
+puts I said : "#{x} "
+puts I also said : "#{y}"
+
+opinion = flase
+review = "Apples are in good condition? #{opinion}"
+
+puts "review"
